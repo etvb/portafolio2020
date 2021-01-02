549ae120042cee1e3e68e2eabfa155744df041a8
linux-kbl <edgardo.torres@hotmail.com>
2021-01-02 07:48:41 -0600

    terminando los media queri

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function cambiarClase(){
+  let siteNav = document.getElementById('site-nav');
+      siteNav.classList.toggle('site-nav-open');
+  let menuOpen = document.getElementById('menu-toggle');
+      menuOpen.classList.toggle('menu-open');    
+      
+}
+
+// https://www.youtube.com/watch?v=uZ7YQInnWqA
+
+// https://www.youtube.com/watch?v=NjVKNWwfK8w
